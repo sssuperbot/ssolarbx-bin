238576c2181aa0b3e477ac4b2da82bc17b64785e
sakarin.s <53126342+sang-sakarin@users.noreply.github.com>
2025-01-13 20:09:40 +0700

    add cron job reset program script (#7)
    * feat: add script setup cron
    
    * fix: cron time
    
    ---------
    
    Co-authored-by: sakkarin <sakkarin.s@comseven.com>

---
+++ setup-cron.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the new cron job
+CRON_JOB="0 0 */3 * * cd $PWD && ./run-screens.sh"
+
+# Debug output
+echo "Clearing previous crontab and adding the new job."
+echo "New cron job: $CRON_JOB"
+
+# Clear the current crontab
+crontab -r 2>/dev/null
+
+# Add the new cron job
+echo "$CRON_JOB" | crontab -
+
+# Verify the addition
+if crontab -l | grep -qF "$CRON_JOB"; then
+    echo "New cron job added successfully."
+else
+    echo "Failed to add the new cron job."
+fi
